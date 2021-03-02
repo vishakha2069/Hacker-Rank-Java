7a3a7f4522a2de31dd3eed6511946158568763f5
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-02 17:53:12 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,35 @@
+# Hacker-Rank-Java
+
+import java.util.Scanner;
+
+public class DataTypes
+{
+    public static void main(String[] args) {
+      
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        int i2;
+        double d2;
+        String s2;
+
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+
+        scan.nextLine();
+        s2 = scan.nextLine();
+
+    
+        System.out.println(i + i2);
+
+        
+        System.out.println(d + d2);
+
+        System.out.println(s.concat(s2));
+
+        scan.close();
+    }
+}
