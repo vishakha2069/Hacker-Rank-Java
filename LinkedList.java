67c5017624bf48c11dfb82ec26d2f0c3e7fbaba0
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-16 19:00:35 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,51 @@
+# Hacker-Rank-Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        Node n = new Node(data);
+        Node temp = head;
+        if (head == null) {
+            head = n;
+            return head;
+        }
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = n;
+        return head;
+    //Complete this method
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
